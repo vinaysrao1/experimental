3f55900ce5233463fc040129427f0eeddd5de297
vinaysrao1 <vinayr@gmail.com>
2024-12-31 16:24:31 -0800

    commit second

---
+++ hello.py
@@ -0,0 +1,9 @@
+# This is a simple Hello World program
+
+def main():
+    print("Hello, World!")
+    print('and this is the second edit')
+    print('and this is the third edit')
+
+if __name__ == "__main__":
+    main()
